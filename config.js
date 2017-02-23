1382d11797517fff1975281733c87c9c78892941
Nokey <bi.jiankun@cctvnews.cn>
2017-02-23 18:03:26 +0800

    modify config

--- config.js
+++ config.js
@@ -10,10 +10,7 @@ var config = {
   redis_host: '127.0.0.1',
   redis_port: 6379,
   redis_db: 0,
-  redis_password: 'nokey458318331',
-
-  // Pagination
-  news_num_per_page: 9
+  redis_password: '123456'
 };
 
 module.exports = config;

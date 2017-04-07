20b1b2f846ad6f89a1df95d98dd3b17b800d3e56
Nokey <bi.jiankun@cgtn.com>
2017-04-07 14:56:52 +0800

    some tiny change

--- models/mongoClient.js
+++ models/mongoClient.js
@@ -2,16 +2,16 @@
 * @Author: Nokey
 * @Date:   2016-09-29 11:44:02
  * @Last Modified by: Nokey
- * @Last Modified time: 2017-02-07 20:43:48
+ * @Last Modified time: 2017-04-07 14:48:21
 */
 
 'use strict';
 
-var mongoose          = require('mongoose');
-mongoose.Promise      = global.Promise;
-var Schema            = mongoose.Schema;
-var config            = require('../config.js');
-var test_conn = mongoose.createConnection(config.db_myapp);
+var mongoose     = require('mongoose');
+mongoose.Promise = global.Promise;
+var Schema       = mongoose.Schema;
+var config       = require('../config.js');
+var test_conn    = mongoose.createConnection(config.db_myapp);
 
 // test conn events
 test_conn.on('error', function(err){
